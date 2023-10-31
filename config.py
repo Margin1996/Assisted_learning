5604037e577936b37142056859d63a05efc88158
WHUlwb <47979237+WHUlwb@users.noreply.github.com>
2023-10-31 11:02:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ config = ed({
     "trainroot": r"data\train", 
     "valroot": r"data\val", 
     "save_root": "model_sar_rgb", 
-    "amp": False, #是否使用fp16,损失计算与梯度计算为半精度
+    "amp": False, # use fp16, loss and gradient calculations are half-precision or not.
     "classnum": 8, #dsm 6 sar 8
     "batch_size": 8, #batch_size
     "lr" : 1e-4, #leanring rate
