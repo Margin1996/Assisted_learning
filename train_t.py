9850016ce6cd7a96537ccedf43aa8ebffe41687a
WHUlwb <47979237+WHUlwb@users.noreply.github.com>
2023-12-16 19:07:06 +0800

    Update train_t.py

--- train_t.py
+++ train_t.py
@@ -18,7 +18,7 @@ scaler = Gradscaler()
 traindd = MyDataset(config.trainroot,is_training=True)
 traindata = DataLoader(traindd,batch_size=config.batch_size, shuffle=True)
 valdata = DataLoader(MyDataset(config.valroot,is_training=False), num_workers=0, batch_size=config.batch_size, shuffle=False)
-net = HRnet(in_channel=3,num_classes=config.classnum,backbone='hrnetv2_w32',pretrained=True).cuda()
+net = HRnet(in_channel=3,num_classes=config.classnum,backbone='hrnetv2_w32').cuda()
 optimizer = torch.optim.SGD(net.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
